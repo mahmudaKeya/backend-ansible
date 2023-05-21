9c7d0944fdc6271a7d85727161fd82ea0b819468
Keya <mahmudakeya013@gmail.com>
2023-05-21 23:01:18 +0600

    adding files for backend

---
+++ configs/config_db.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT_PATH = os.getcwd()
+DATA_PATH = "DATA"
+DATABASE_NAME = 'company_data'
+USER_NAME = 'scraper'
+PASSWORD = '123'
+HOST_NAME = 'localhost'
+TABLE_NAME = 'all_company_data'
---
+++ main.py
@@ -0,0 +1,3 @@
+from db.db import DB
+
+DB()
